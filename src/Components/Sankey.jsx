2f4700e7684e9585db71ba27c3425ff9a904d829
vmadhuuu <madhuabi19@gmail.com>
2023-11-21 22:22:16 -0800

    code restructure and added scrolling feature

--- src/Components/Sankey.jsx
+++ src/Components/Sankey.jsx
@@ -1,130 +1,189 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
 import { sankey, sankeyLinkHorizontal, sankeyJustify } from "d3-sankey";
 
-function SankeyChart({ data }) {
+function SankeyChart() {
   const svgRef = useRef();
-  const [activeNode, setActiveNode] = useState(null);
-
-  //   const highlightConnectedPaths = (node) => {
-  //     const isActive = node !== activeNode;
-  //     setActiveNode(isActive ? node : null);
-
-  //     d3.selectAll(".link")
-  //       .transition()
-  //       .duration(300)
-  //       .style("stroke-opacity", (d) => {
-  //         return isActive && (d.source === node || d.target === node)
-  //           ? 0.7
-  //           : 0.05;
-  //       })
-  //       .style("stroke", (d) => {
-  //         return isActive && (d.source === node || d.target === node)
-  //           ? "orange"
-  //           : "grey";
-  //       });
-  //   };
+  const [sankeyData, setSankeyData] = useState({ nodes: [], links: [] });
 
   useEffect(() => {
-    if (!data || !data.nodes || !data.links) {
-      console.error("Sankey chart data is missing or in an incorrect format");
-      return;
-    }
-
-    const svg = d3.select(svgRef.current);
-    const width = 1000; // Adjust width and height as needed
-    const height = 800;
-    svg.attr("width", width).attr("height", height);
-
-    svg.selectAll("*").remove(); // Clear any previous SVG content
-
-    // color gradient
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    const defs = svg.append("defs");
-
-    data.links.forEach((link, i) => {
-      const gradient = defs
-        .append("linearGradient")
-        .attr("id", "link-gradient-" + i)
-        .attr("gradientUnits", "userSpaceOnUse")
-        .attr("x1", link.source.x0)
-        .attr("y1", (link.source.y0 + link.source.y1) / 2)
-        .attr("x2", link.target.x0)
-        .attr("y2", (link.target.y0 + link.target.y1) / 2);
-
-      gradient
-        .append("stop")
-        .attr("offset", "0%")
-        .attr("stop-color", color(link.source.name));
-
-      gradient
-        .append("stop")
-        .attr("offset", "100%")
-        .attr("stop-color", color(link.target.name));
-    });
-
-    try {
-      const sankeyGenerator = sankey()
-        .nodeWidth(15)
-        .nodePadding(10)
-        .extent([
-          [1, 1],
-          [width - 1, height - 5],
-        ])
-        .nodeAlign(sankeyJustify);
-
-      const graph = sankeyGenerator({
-        nodes: data.nodes.map((d) => ({ ...d })),
-        links: data.links.map((d) => ({ ...d, value: Math.max(d.value, 0) })),
+    d3.csv("../data/vgsalessample.csv").then((loadedData) => {
+      //console.log(loadedData);
+      const cleanedData = loadedData.filter((row) => {
+        return !Object.values(row).some(
+          (value) => value === null || value === ""
+        );
       });
 
-      // Nodes
-      const node = svg
-        .append("g")
-        .selectAll("rect")
-        .data(graph.nodes)
-        .enter()
-        .append("rect")
-        .attr("class", "node")
-        .attr("x", (d) => d.x0)
-        .attr("y", (d) => d.y0)
-        .attr("height", (d) => d.y1 - d.y0)
-        .attr("width", sankeyGenerator.nodeWidth())
-        .attr("fill", (d) => color(d.name))
-        .on("click", (event, d) => highlightConnectedPaths(d));
+      //console.log(cleanedData);
+      function processDataForSankey(data) {
+        const extractTop = (data, key) => {
+          let totals = data.reduce((acc, item) => {
+            const name = item[key];
+            const sales = parseFloat(item.Global_Sales || 0);
+            acc[name] = (acc[name] || 0) + sales;
+            return acc;
+          }, {});
 
-      // Links
-      const link = svg
-        .append("g")
-        .attr("fill", "none")
-        .selectAll("path")
-        .data(graph.links)
-        .enter()
-        .append("path")
-        .attr("class", "link") // Ensure this class matches the selector used in highlightConnectedPaths
-        .attr("d", sankeyLinkHorizontal())
-        .attr("stroke", (d, i) => `url(#link-gradient-${i})`) // Use template literals for correct string formation
-        .attr("stroke-width", (d) => Math.max(1, d.width));
+          return Object.entries(totals)
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, 10)
+            .map((item) => item[0]);
+        };
 
-      // Text Labels
-      svg
-        .append("g")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 12)
-        .selectAll("text")
-        .data(graph.nodes)
-        .enter()
-        .append("text")
-        .attr("x", (d) => (d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6))
-        .attr("y", (d) => (d.y1 + d.y0) / 2)
-        .attr("dy", "0.35em")
-        .attr("text-anchor", (d) => (d.x0 < width / 2 ? "start" : "end"))
-        .text((d) => d.name);
-    } catch (error) {
-      console.error("Error in Sankey chart rendering:", error);
-    }
-  }, [data, activeNode]);
+        const topPublishers = extractTop(data, "Publisher");
+        const topPlatforms = extractTop(data, "Platform");
+        const topGenres = extractTop(data, "Genre");
+
+        // Combine and deduplicate
+        const combinedTop = [
+          ...new Set([...topPublishers, ...topPlatforms, ...topGenres]),
+        ];
+
+        const nodes = combinedTop.map((name) => ({ name }));
+
+        // Debugging: Check the nodes
+        //console.log("Nodes:", nodes);
+
+        const links = [];
+        data.forEach((item) => {
+          const publisher = item.Publisher;
+          const platform = item.Platform;
+          const genre = item.Genre;
+          const value = parseFloat(item.Global_Sales || 0);
+
+          if (
+            topPublishers.includes(publisher) &&
+            topPlatforms.includes(platform) &&
+            topGenres.includes(genre) &&
+            value > 0
+          ) {
+            const publisherIndex = nodes.findIndex(
+              (node) => node.name === publisher
+            );
+            const platformIndex = nodes.findIndex(
+              (node) => node.name === platform
+            );
+            const genreIndex = nodes.findIndex((node) => node.name === genre);
+
+            if (publisherIndex !== -1 && platformIndex !== -1) {
+              links.push({
+                source: publisherIndex,
+                target: platformIndex,
+                value,
+              });
+            }
+            if (platformIndex !== -1 && genreIndex !== -1) {
+              links.push({ source: platformIndex, target: genreIndex, value });
+            }
+          }
+        });
+
+        // Debugging: Check the links
+        // console.log("Links:", links);
+
+        return { nodes, links };
+      }
+
+      const processedData = processDataForSankey(cleanedData);
+      // console.log(processedData);
+
+      setSankeyData(processedData);
+    });
+  }, []);
+
+  //console.log(sankeyData);
+
+  if (!sankeyData || !sankeyData.nodes || !sankeyData.links) {
+    console.error("Sankey chart data is missing or in an incorrect format");
+    return;
+  }
+
+  //    Debugging logs
+  // console.log("Nodes:", sankeyData.nodes);
+  // console.log("Links:", sankeyData.links);
+
+  const uniqueNodes = new Set(sankeyData.nodes.map((node) => node.name));
+  if (uniqueNodes.size !== sankeyData.nodes.length) {
+    console.error("Duplicate nodes detected");
+    return;
+  }
+
+  // Check for invalid link indices
+  const invalidLinks = sankeyData.links.filter(
+    (link) =>
+      link.source < 0 ||
+      link.source >= sankeyData.nodes.length ||
+      link.target < 0 ||
+      link.target >= sankeyData.nodes.length
+  );
+  if (invalidLinks.length > 0) {
+    console.error("Invalid links detected:", invalidLinks);
+    return;
+  }
+
+  const svg = d3.select(svgRef.current);
+  const width = 1000; // Adjust width and height as needed
+  const height = 800;
+  svg.attr("width", width).attr("height", height);
+
+  try {
+    const sankeyGenerator = sankey()
+      .nodeWidth(15)
+      .nodePadding(10)
+      .extent([
+        [1, 1],
+        [width - 1, height - 5],
+      ])
+      .nodeAlign(sankeyJustify);
+
+    const graph = sankeyGenerator({
+      nodes: sankeyData.nodes.map((d) => ({ ...d })),
+      links: sankeyData.links.map((d) => ({
+        ...d,
+        value: Math.max(d.value, 0),
+      })),
+    });
+
+    svg
+      .append("g")
+      .selectAll("rect")
+      .data(graph.nodes)
+      .enter()
+      .append("rect")
+      .attr("x", (d) => d.x0)
+      .attr("y", (d) => d.y0)
+      .attr("height", (d) => d.y1 - d.y0)
+      .attr("width", sankeyGenerator.nodeWidth())
+      .attr("fill", "blue"); // Modify as needed
+
+    svg
+      .append("g")
+      .attr("fill", "none")
+      .selectAll("path")
+      .data(graph.links)
+      .enter()
+      .append("path")
+      .attr("d", sankeyLinkHorizontal())
+      .attr("stroke", "grey") // Modify as needed
+      .attr("stroke-width", (d) => Math.max(1, d.width));
+
+    svg
+      .append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .selectAll("text")
+      .data(graph.nodes)
+      .enter()
+      .append("text")
+      .attr("x", (d) => (d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6))
+      .attr("y", (d) => (d.y1 + d.y0) / 2)
+      .attr("dy", "0.35em")
+      .attr("text-anchor", (d) => (d.x0 < width / 2 ? "start" : "end"))
+      .text((d) => d.name);
+  } catch (error) {
+    console.error("Error in Sankey chart rendering:", error);
+  }
 
   return (
     <div className="sankeyContainer">
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,21 +3,24 @@ import "../App.css";
 
 function App() {
   return (
-    <div className="container">
-      <header>
+    <main>
+      <section className="container">
         <h1>Madhumitha and Apoorva present</h1>
-      </header>
-      <section className="content">
-        <p className="intro-text">Video Game Dashboard</p>
-        <div className="scroll-instruction">Scroll to Continue</div>
       </section>
-      <div
-        className="character"
-        style={{ backgroundImage: "src/data/Elements/mario.png" }}
-      >
-        {/* The character's sprite will be displayed here */}
-      </div>
-    </div>
+
+      <section className="content">
+        <p>Video Game Dashboard</p>
+      </section>
+
+      <section id="character">
+        <h1> insert character here</h1>
+      </section>
+      {/* The character's sprite will be displayed here */}
+
+      <section className="footer">
+        <h1>Let's end the application with this Footer</h1>
+      </section>
+    </main>
   );
 }
 
