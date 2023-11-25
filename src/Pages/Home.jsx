df5c8515d9b5eb0d425a6f205c9ae08a4f2f0aa3
vmadhuuu <madhuabi19@gmail.com>
2023-11-25 12:00:43 -0800

    finished 90% of 1st slide

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,25 +51,59 @@ function App() {
             alt="textbox"
             className="speechbubble"
           />
-          {/* <div className="title-text">
-            <p> Let's Play Pixel Pulse! </p>
-          </div> */}
+          <div className="title-textbox">
+            <p className="title-subtitle">
+              👾 Dive into a dynamic journey through the vibrant universe of
+              video games. Explore captivating visuals and intriguing data as we
+              unravel the trends, patterns, and secrets that define today's
+              gaming landscape. From the pixelated realms of retro classics to
+              the immersive universes of modern blockbusters, our dashboard
+              offers a unique lens to view the ever-evolving world of video
+              games. Get ready to level up your understanding with engaging
+              analytics and fun facts! Embark on this adventure with us, and
+              discover the hidden gems in the gaming galaxy! 👾
+            </p>
+            <p className="title-text">
+              <b> Let's Play Pixel Pulse! </b>
+            </p>
+          </div>
+          <img
+            src="src/Elements/cloud.png"
+            alt="Cloud"
+            className="moving-cloud"
+          />
+
+          <img
+            src="src/Elements/sun.png"
+            alt="Rotating Sun"
+            class="rotating-sun sun1"
+          />
+          <img
+            src="src/Elements/sun.png"
+            alt="Rotating Sun"
+            class="rotating-sun sun2"
+          />
+          <img
+            src="src/Elements/sun.png"
+            alt="Rotating Sun"
+            class="rotating-sun sun3"
+          />
 
           <button onClick={() => scrollToSection("#second-slide")}>
             Go to Second Slide
           </button>
         </section>
 
-        <section className="content" id="second-slide" data-scroll-section>
+        <section className="second" id="second-slide" data-scroll-section>
           <p>Video Game Dashboard</p>
         </section>
 
-        <section id="character" data-scroll-section>
+        <section id="third" data-scroll-section>
           <h1> insert character here</h1>
         </section>
         {/* The character's sprite will be displayed here */}
 
-        <section className="footer" data-scroll-section>
+        <section className="fourth" data-scroll-section>
           <h1>Let's end the application with this Footer</h1>
         </section>
       </main>
