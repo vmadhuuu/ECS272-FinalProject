45fd0793e41babed41732313a7a31b3e4991288d
CodeBuildder <kumarankaushik@gmail.com>
2023-11-25 20:17:07 -0500

    locomotive added

---
+++ src/Components/SecondPage.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
+
+const SecondPage = () => {
+  const [backgroundColor, setBackgroundColor] = useState("#FFFFFF");
+  const { scroll } = useLocomotiveScroll();
+
+  useEffect(() => {
+    if (scroll) {
+      scroll.on("scroll", (instance) => {
+        const newColor = instance.scroll.y > 500 ? "#FF0000" : "#FFFFFF";
+        setBackgroundColor(newColor);
+      });
+    }
+
+    return () => {
+      if (scroll) {
+        scroll.destroy();
+      }
+    };
+  }, [scroll]);
+
+  return (
+    <div
+      style={{
+        backgroundColor: backgroundColor,
+        transition: "background-color 300ms ease",
+      }}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </div>
+  );
+};
+
+export default SecondPage;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import {
 import React, { useRef, useCallback, useContext } from "react";
 import "../App.css";
 import backgroundImg from "../Elements/classic-world-video-game-background-free-vector.jpg";
+import SecondPage from "../Components/SecondPage";
 
 function App() {
   const containerRef = useRef(null);
@@ -31,21 +32,30 @@ function App() {
 
   //   // scrollRef.current.scrollTo(section);
   // }, []);
+  const options = {
+    smooth: true,
+  };
 
   return (
-    <LocomotiveScrollProvider containerRef={containerRef} watch={[]}>
-      <main data-scroll-container ref={containerRef}>
+    <LocomotiveScrollProvider
+      options={options}
+      containerRef={containerRef}
+      watch={[]}
+    >
+      <main data-scroll-container options={options} ref={containerRef}>
         <section
           className="container"
-          style={{
-            backgroundImage: `url(${backgroundImg})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
+          // style={{
+          //   backgroundImage: `url(${backgroundImg})`,
+          //   backgroundSize: "cover",
+          //   backgroundPosition: "center",
+          // }}
           id="first-slide"
+          data-scroll-speed="4"
           data-scroll-section
         >
-          <img src="src/Elements/mario.png" alt="Mario" className="mario" />
+          <h1 data-scroll>hu</h1>
+          {/* <img src="src/Elements/mario.png" alt="Mario" className="mario" />
           <img
             src="src/Elements/speechbubble1.png"
             alt="textbox"
@@ -86,25 +96,99 @@ function App() {
             src="src/Elements/sun.png"
             alt="Rotating Sun"
             class="rotating-sun sun3"
-          />
+          /> */}
 
           {/* <button onClick={() => scrollToSection("#second-slide")}>
             Go to Second Slide
           </button> */}
         </section>
-
-        <section className="second" id="second-slide" data-scroll-section>
-          <p>Video Game Dashboard</p>
+        <section className="contents" data-scroll-section>
+          <h1
+            className="op-class"
+            data-scroll
+            style={{ fontSize: 172 }}
+            data-scroll-class="fadeIn"
+            data-scroll-repeat="true"
+            data-scroll-speed="1"
+          >
+            How Data Data Data Data Data?
+          </h1>
         </section>
 
-        <section id="third" data-scroll-section>
-          <h1> insert character here</h1>
+        <section className="contents" id="second-slide" data-scroll-section>
+          <h1
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="9"
+          >
+            Content for something here
+          </h1>
+          <h2
+            data-scroll
+            data-scroll-direction="vertical"
+            data-scroll-speed="9"
+          >
+            hihihi
+          </h2>
+          <h4>iuub</h4>
+        </section>
+
+        <section id="stick" className="contents" data-scroll-section>
+          <h1
+            data-scroll
+            data-scroll-speed="5"
+            data-scroll-sticky // Attibute that enables the sticky scroll
+            data-scroll-target="#stick"
+          >
+            Bar Chart content here
+          </h1>
+
+          <p>other contents</p>
+          <p>other contents</p>
+          <p>other contents</p>
+        </section>
+
+        <section className="contents" data-scroll-section>
+          <h1
+            className="op-class"
+            data-scroll
+            data-scroll-class="fadeIn"
+            data-scroll-direction="horizontal"
+            data-scroll-speed="9"
+          >
+            How Data Data Data Data Data?
+          </h1>
+        </section>
+
+        <section className="contents" data-scroll-section>
+          <h1
+            className="op-class"
+            data-scroll
+            data-scroll-class="fadeIn"
+            data-scroll-direction="horizontal"
+            data-scroll-speed="9"
+          >
+            How Data Data Data Data Data?
+          </h1>
+        </section>
+        <section className="contents" id="second-slide" data-scroll-section>
+          <h1
+            data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="9"
+          >
+            Content for something here
+          </h1>
+          <h2
+            data-scroll
+            data-scroll-direction="vertical"
+            data-scroll-speed="9"
+          >
+            hihihi
+          </h2>
+          <h4>iuub</h4>
         </section>
         {/* The character's sprite will be displayed here */}
-
-        <section className="fourth" data-scroll-section>
-          <h1>Let's end the application with this Footer</h1>
-        </section>
       </main>
     </LocomotiveScrollProvider>
   );
